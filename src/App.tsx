06367a7a6918a8b223b2da851acad8bdd1f7f697
Inna Dmytrenko <inna.shalun@gmail.com>
2024-10-08 22:17:40 +0300

    added styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,59 @@
-import React, { useState } from 'react';
-import styles from "./components/Site.module.css";
 import { PageOne } from './components/pages/PageOne';
 import { PageTwo } from './components/pages/PageTwo';
 import { PageThree } from './components/pages/PageThree';
 import { Navigate, NavLink, Route, Routes } from 'react-router-dom';
 import { Error404 } from './components/pages/Error404';
+import { S } from './components/pages/_styles';
 
+const PATH = {
+    PAGE1: 'page1',
+    PAGE2: 'page2',
+    PAGE3: 'page3',
+    PAGEERROR: '/error404'
+} as const
 
 function App() {
     return (
         <div>
-            <div className={styles.header}><h1>HEADER</h1></div>
-            <div className={styles.body}>
-                <div className={styles.nav}>
-                    <div><NavLink to={"/page1"} className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>
-                        Page 1</NavLink></div>
-                    <div><NavLink to={"/page2"} className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>
-                        Page 2</NavLink></div>
-                    <div><NavLink to={"/page3"} className={({ isActive }) => isActive ? styles.activeNavLink : styles.navLink}>
-                        Page 3</NavLink></div>
-                </div>
-                <div className={styles.content}>
+            <S.Header><h1>HEADER</h1></S.Header>
+            <S.Body>
+                <S.Nav>
+                    <S.NavWrapper><NavLink to={PATH.PAGE1} >
+                        Page 1</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE2} >
+                        Page 2</NavLink></S.NavWrapper>
+                    <S.NavWrapper><NavLink to={PATH.PAGE3} >
+                        Page 3</NavLink></S.NavWrapper>
+                    <a href='page3'>page3 HTML</a>
+                </S.Nav>
+                <S.Content>
                     <Routes>
                         <Route
                             path="/"
-                            element={<Navigate to={"/page1"} />} />
+                            element={<Navigate to={PATH.PAGE1} />} />
                         <Route
-                            path="/page1"
+                            path={PATH.PAGE1}
                             element={<PageOne />} />
                         <Route
-                            path="/page2"
+                            path={PATH.PAGE2}
                             element={<PageTwo />} />
                         <Route
-                            path="/page3"
+                            path={PATH.PAGE3}
                             element={<PageThree />} />
 
                         <Route
-                            path="/error404"
+                            path={PATH.PAGEERROR}
                             element={<Error404 />} />
                         <Route
                             path="/*"
-                            element={<Navigate to={"/error404"} />} />
+                            element={<Navigate to={PATH.PAGEERROR} />} />
                     </Routes>
-                </div>
-            </div>
-            <div className={styles.footer}>abibas 2023</div>
-        </div>
+                </S.Content>
+            </S.Body>
+            <S.Footer>abibas 2023</S.Footer>
+        </div >
     );
 }
 
-
 export default App;
+
